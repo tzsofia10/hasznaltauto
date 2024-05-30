419e8e50682a5dfc4801eb151a6c8a392151274a
TothZsofia <tzsofia07@gmail.com>
2024-05-30 14:03:50 +0200

    Add project files.

---
+++ hasznaltauto/Car.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hasznaltauto
+{
+    internal class Car
+    {
+        public string Nev { get; set; }
+
+        public string Tipus { get; set;}
+
+        public string Uzemanyag { get; set; }
+
+        public int Ev { get; set; }
+     
+        public string Kep { get; set; }
+
+        public Car(string Nev, string Tipus, string Uzemanyag, int Ev, string source) 
+        {
+            Nev = Nev;
+            Tipus = Tipus;
+            Uzemanyag = Uzemanyag;
+            Tipus = Tipus;
+            Kep = $@"..\..\..\imges\{source}";
+        }
+
+    }
+
+
+}
